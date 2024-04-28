60b780b596d8cbe581b1dcf4833a0eeb8435cbdc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-04-28 01:24:22 -0300

    create index page

--- src/test/java/me/calebeoliveira/spring5recipeapp/Spring5RecipeAppApplicationTests.java
+++ src/test/java/me/calebeoliveira/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -1,13 +1,13 @@
 package me.calebeoliveira.spring5recipeapp;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring5RecipeAppApplicationTests {
+public class Spring5RecipeAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
