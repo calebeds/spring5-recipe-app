401494e12d810cebc782d9384905764b29665dcc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-07 21:00:49 -0300

    add ingredient and implement oneToMany relationship

--- src/main/java/me/calebeoliveira/spring5recipeapp/domain/Recipe.java
+++ src/main/java/me/calebeoliveira/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package me.calebeoliveira.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -20,6 +21,8 @@ public class Recipe {
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
