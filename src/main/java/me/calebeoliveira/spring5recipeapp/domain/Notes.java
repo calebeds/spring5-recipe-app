c56729e270aa22e6de688c24c0104ba93ea3a2c0
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-08 10:43:05 -0300

    add category and ManyToMany relationship between Recipe and Category

--- src/main/java/me/calebeoliveira/spring5recipeapp/domain/Notes.java
+++ src/main/java/me/calebeoliveira/spring5recipeapp/domain/Notes.java
@@ -2,6 +2,7 @@ package me.calebeoliveira.spring5recipeapp.domain;
 
 import javax.persistence.*;
 
+@Entity
 public class Notes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/me/calebeoliveira/spring5recipeapp/domain/Recipe.java
+++ src/main/java/me/calebeoliveira/spring5recipeapp/domain/Recipe.java
@@ -23,6 +23,11 @@ public class Recipe {
     private Notes notes;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -118,5 +123,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
