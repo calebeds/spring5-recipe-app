1420e868ccfe9b34faf9c8b4113065e595f7ec47
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-05-13 15:19:26 -0300

    add junit test for some category attributes

--- src/test/java/me/calebeoliveira/spring5recipeapp/Spring5RecipeAppApplicationTests.java
+++ src/test/java/me/calebeoliveira/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -1,8 +1,11 @@
 package me.calebeoliveira.spring5recipeapp;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
 
