64f2ceafa3ae0e0744a5cc4ed569b177678f1b94
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-06-30 21:11:50 -0300

    implement displaying remaining recipe properties

--- src/main/java/me/calebeoliveira/spring5recipeapp/bootstrap/Bootstrap.java
+++ src/main/java/me/calebeoliveira/spring5recipeapp/bootstrap/Bootstrap.java
@@ -153,6 +153,10 @@ public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
 
+        guacRecipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipes");
+
         //add to return list
         recipes.add(guacRecipe);
 
