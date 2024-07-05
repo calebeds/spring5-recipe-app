1a7a477e5354ab706ad131dda227c83aa9527cc0
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-07-04 22:03:50 -0300

    implement recipe creation

--- src/main/java/me/calebeoliveira/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/me/calebeoliveira/spring5recipeapp/controllers/RecipeController.java
@@ -1,12 +1,11 @@
 package me.calebeoliveira.spring5recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import me.calebeoliveira.spring5recipeapp.commands.RecipeCommand;
 import me.calebeoliveira.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -26,5 +25,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping( "/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
