4e0eb894e67d173a508b1e7e2a9e7bd8412de616
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-07-20 17:45:25 -0300

    add delete ingredient feature

--- src/main/java/me/calebeoliveira/spring5recipeapp/services/IngredientService.java
+++ src/main/java/me/calebeoliveira/spring5recipeapp/services/IngredientService.java
@@ -6,4 +6,6 @@ public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long id);
 
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(long recipeId, long ingredientId);
 }
