a79ebef4f24ec0a6b190313812639082e1144062
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-09-02 21:38:37 -0300

    add mysql script

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,31 @@
+#create databases
+CREATE DATABASE calebe_dev;
+CREATE DATABASE calebe_prod;
+
+# create database service accounts
+CREATE USER 'calebe_dev_user'@'localhost' IDENTIFIED BY 'calebe';
+CREATE USER 'calebe_prod_user'@'localhost' IDENTIFIED BY 'calebe';
+
+# Database grants
+GRANT SELECT ON calebe_dev.* to 'calebe_dev_user'@'localhost';
+GRANT INSERT ON calebe_dev.* to 'calebe_dev_user'@'localhost';
+GRANT DELETE ON calebe_dev.* to 'calebe_dev_user'@'localhost';
+GRANT UPDATE ON calebe_dev.* to 'calebe_dev_user'@'localhost';
+GRANT SELECT ON calebe_prod.* to 'calebe_prod_user'@'localhost';
+GRANT INSERT ON calebe_prod.* to 'calebe_prod_user'@'localhost';
+GRANT DELETE ON calebe_prod.* to 'calebe_prod_user'@'localhost';
+GRANT UPDATE ON calebe_prod.* to 'calebe_prod_user'@'localhost';
+
+# To get connection via docker
+
+CREATE USER 'calebe_dev_user'@'%' IDENTIFIED BY 'calebe';
+CREATE USER 'calebe_prod_user'@'%' IDENTIFIED BY 'calebe';
+
+GRANT SELECT ON calebe_dev.* to 'calebe_dev_user'@'%';
+GRANT INSERT ON calebe_dev.* to 'calebe_dev_user'@'%';
+GRANT DELETE ON calebe_dev.* to 'calebe_dev_user'@'%';
+GRANT UPDATE ON calebe_dev.* to 'calebe_dev_user'@'%';
+GRANT SELECT ON calebe_prod.* to 'calebe_prod_user'@'%';
+GRANT INSERT ON calebe_prod.* to 'calebe_prod_user'@'%';
+GRANT DELETE ON calebe_prod.* to 'calebe_prod_user'@'%';
+GRANT UPDATE ON calebe_prod.* to 'calebe_prod_user'@'%';
